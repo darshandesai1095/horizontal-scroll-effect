c3285b4f96a3b0e324b64a95e1cb3eebbf187f80
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-05-05 18:12:48 +0100

    create image card

---
+++ src/components/ImageCards/ImageCard1/ImageCard1.js
@@ -0,0 +1,16 @@
+import './ImageCard1.css';
+
+function ImageCard1(props) { 
+
+  return (
+    <div className="image-card-1" >
+        <p>Sint ipsum do reprehenderit pariatur fugiat et tempor voluptate nostrud elit est ex.</p>
+        <img src="https://images.unsplash.com/photo-1682685797828-d3b2561deef4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60"/>
+    </div>
+  );
+}
+
+export default ImageCard1;
+
+
+
