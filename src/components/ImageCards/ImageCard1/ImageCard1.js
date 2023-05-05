78fc628eb1e1ab9d22ffdf922e73c10f00b44702
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-05-05 20:32:50 +0100

    add image card

--- src/components/ImageCards/ImageCard1/ImageCard1.js
+++ src/components/ImageCards/ImageCard1/ImageCard1.js
@@ -1,13 +1,54 @@
+import { useState, useEffect, useRef } from 'react';
 import './ImageCard1.css';
 
 function ImageCard1(props) { 
 
-  return (
-    <div className="image-card-1" >
-        <p>Sint ipsum do reprehenderit pariatur fugiat et tempor voluptate nostrud elit est ex.</p>
-        <img src="https://images.unsplash.com/photo-1682685797828-d3b2561deef4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60"/>
-    </div>
-  );
+
+    // get width of image and set width of p to image width
+    ///////////////////////////////////////////////////////
+
+    const image = useRef(null)
+    const [imageWidth, setImageWidth] = useState(0)
+    const [style, setStyle] = useState({})
+
+    useEffect(() => {
+        setImageWidth(image.current.offsetWidth)
+        setStyle({
+            width: `${imageWidth}px`
+        })
+
+    }, [imageWidth])
+
+    ///////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////
+
+
+    // create animation to crop text-box
+    /////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////
+
+
+    return (
+        <div className="ic1" style={style}>
+
+            <img className='ic1__image'
+                 ref = {image}
+                 src="https://images.unsplash.com/photo-1682685797828-d3b2561deef4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60"
+            />
+
+            <div className="ic1__text-bounding" 
+                //  style={{ clipPath: `polygon(0 0, ${50}% 0, ${50}% 100%, 0 100%)` }}
+                 >
+                <p >
+                    Sint ipsum do reprehenderit pariatur fugiat
+                    et tempor voluptate nostrud elit est ex.
+                </p>
+            </div>
+
+
+        </div>
+    )
 }
 
 export default ImageCard1;
