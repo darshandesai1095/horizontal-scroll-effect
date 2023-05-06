f6310a788c39c9fd1d4670739ee8ef26b5584ee6
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-05-06 16:14:45 +0100

    add horizontal motion on vertical scroll functionaluty

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
+import { useRef, useState } from 'react';
 import Animation1 from './components/Animations/Animation1/Animation1';
-import ImageCard1 from './components/ImageCards/ImageCard1/ImageCard1';
+import NavBar from './components/NavBar/NavBar';
 
-function App() { 
-  
-  // refresh screen if resize detected
+function App() {  // refresh screen if resize detected
+
+  const appRef = useRef(null)
+  const [xOffset, setXoffset] = useState()
+  const handleScroll = (event) => {
+    // event.preventDefault()
+    appRef.current.scrollLeft += event.deltaY
+    setXoffset(appRef.current.scrollLeft)
+  }
 
   return (
-    <div className="App" >
-      <Animation1/>
-      {/* <ImageCard1/> */}
+    <div className="App" 
+         ref={appRef}
+         onWheel={handleScroll}
+         style={{ overflowX: "scroll"}}
+    >
+      <NavBar/>
+      <Animation1 xOffset={xOffset || 0}/>
     </div>
   );
 }
