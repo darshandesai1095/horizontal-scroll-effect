9213f2c69b968caf673f69ef3ce4449f3c08f61b
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-05-06 16:16:11 +0100

    add static navbar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import './NavBar.css';
+
+function NavBar() {
+
+    return (
+        <div className="nav-bar">
+            <p className='nav-bar__logo'>〄</p>
+            <p className='menu-icon'>≡</p>
+            <p className='random-description'>RANDOM DESCRIPTION</p>
+        </div>
+    )
+}
+
+export default NavBar;
+
+
