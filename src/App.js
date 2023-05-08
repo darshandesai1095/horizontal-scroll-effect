cd587ee303c530619f39e01efd8acd0083b3db6d
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-05-08 18:33:01 +0100

    add test articles

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import { useRef, useState, useEffect } from "react";
 import "./App.css";
+import UnsplashImages from "./TEST/TESTFILE.js"
+import Article from "./components/Article/Article";
 
 const App = () => {
 
-  const appRef = useRef(null)
-  const [xOffset, setXOffset] = useState(0)
-
-  const handleScroll = (event) => {
-    console.log(appRef.current.scrollLeft)
-    appRef.current.scrollLeft += event.deltaY
-    setXOffset(appRef.current.scrollLeft)
-  }
 
   return (
-    <div className="App" ref={appRef} onWheel={handleScroll}
-         style={{ overflowX: "scroll" }}>
-      <div className="content">
-      
+    <div className="App">
+      <div className="main-content">
+        <Article val={0}/>
+        <Article val={1}/>
+        <Article val={2}/>
+        <Article val={3}/>
+        <Article val={4}/>
+        <Article val={5}/>
+        <Article val={6}/>
+        <Article val={7}/>
+        <Article val={8}/>
+        <Article val={9}/>
+        <Article val={10}/>
+        <Article val={11}/>
+        <Article val={12}/>
+        <Article val={13}/>
+        <Article val={14}/>
       </div>
     </div>
   );
