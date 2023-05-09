6e53ec34d94443443dfd8d89497e73693dcedbde
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-05-09 21:03:52 +0100

    cleanup

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -4,7 +4,7 @@ import "./Image.css";
 const Image = (props) => {
 
     const height = useMemo(() => {
-        const imgHeights = [30, 40, 50, 60, 70, 80]
+        const imgHeights = [35, 40, 50, 60, 70, 80]
         const rand = Math.floor(Math.random() * imgHeights.length)
         return imgHeights[rand]
     }, [])
